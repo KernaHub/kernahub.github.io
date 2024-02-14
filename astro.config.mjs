eeb39cf622f99ada4da6a578e326da41b1153fb6
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-02-14 07:41:31 +0000

    puush

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Kernadoc',
+			social: {
+				github: 'https://github.com/KernaHub/kernadoc',
+			},
+			sidebar: [
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
