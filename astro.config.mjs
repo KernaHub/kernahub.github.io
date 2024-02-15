ea6f3fdc26b824d1272e1b8af5678bd2cdcf3b71
Funasitien DreamCloud <86372093+Funasitien@users.noreply.github.com>
2024-02-15 07:55:11 +0000

    🎨 Hmmm design is here

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,23 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://kernahub.github.io',
 	integrations: [
 		starlight({
 			title: 'Kernadoc',
+			customCss: [
+				// Fichiers Fontsource pour les graisses regular et semi-bold.
+				'@fontsource/jetbrains-mono/400.css',
+				'@fontsource/jetbrains-mono/600.css',
+				'/src/styles/custom.css',
+			],
 			social: {
 				github: 'https://github.com/KernaHub/kernadoc',
 			},
 			sidebar: [
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Informations',
+					autogenerate: { directory: 'info' },
 				},
 			],
 		}),
